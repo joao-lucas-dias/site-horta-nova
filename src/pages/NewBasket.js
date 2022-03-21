79ef06d6fe42624cc117eef7238593efac6eaa56
joao-lucas-dias <joao.lucasdias@hotmail.com>
2022-03-21 18:36:11 +0000

    Update NewBasket.js

--- src/pages/NewBasket.js
+++ src/pages/NewBasket.js
@@ -2,6 +2,9 @@ import { Fragment } from "react";
 import CategoriesList from "../components/products/CategoriesList";
 import ProductsList from "../components/products/ProductsList";
 
+// This is for development purposes only. Deploy app should get this file from database
+import data from "../assets/lista_produtos.json";
+
 const productsInfo = JSON.parse(data);
 
 console.log(productsInfo);
@@ -10,6 +13,8 @@ const NewBasket = () => {
   return (
     <Fragment>
       <h1>Personalize o seu Cabaz</h1>
+      <CategoriesList categories={data.categories}/>
+      <ProductsList products={data.products}/>
     </Fragment>
   );
 };
